9e5ac2d051d5b236129dc859157b88563e1a7eaa
IVaN-KaLNYTSKYI <ivan.kalnytskyi@gmail.com>
2021-06-24 21:44:22 +0300

    hm-2

--- hm-2/app.js
+++ hm-2/app.js
@@ -5,11 +5,9 @@ const fs = require('fs');
 
 const app = express();
 
-
 app.use(express.static(path.join(__dirname, 'static')));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
 
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'static'));
@@ -20,11 +18,10 @@ app.engine('.hbs', expressHbs({
 
 const pathFile = path.join(__dirname, 'db', 'db.json');
 
-const data=fs.readFileSync(pathFile);
+const data = fs.readFileSync(pathFile);
 const allUsers = JSON.parse(data.toString());
 
 /*let dataFile=null
-
 fs.readFile(pathFile, (err, data) => {
     if (err) {
         console.log(err);
@@ -61,21 +58,20 @@ app.post('/register', (req, res) => {
     const {email} = req.body;
     const flag = allUsers.some((value => value.email === email));
 
-
-    if (flag) res.redirect('/error');
-
-    if (!flag) {
-        const users = [];
-
-        allUsers.forEach((value1) => {
-            users.push(value1);
-        })
-        users.push(req.body);
-
-        fs.writeFile(pathFile, JSON.stringify(users), (err => err && console.log(err)));
-
-        res.redirect('/login');
+    if (flag) {
+        res.redirect('/error');
+        return;
     }
+    const users = [];
+
+    allUsers.forEach((value1) => {
+        users.push(value1);
+    })
+    users.push(req.body);
+
+    fs.writeFile(pathFile, JSON.stringify(users), (err => err && console.log(err)));
+
+    res.redirect('/login');
 });
 
 app.post('/login', (req, res) => {
@@ -83,9 +79,11 @@ app.post('/login', (req, res) => {
     const flag = allUsers.some((value => value.email === email && value.password === password));
     const user = allUsers.find((value => value.email === email));
 
-    if (flag) res.redirect(`/user/${user.email}`);
-
-    if (!flag) res.redirect('/register');
+    if (flag) {
+        res.redirect(`/user/${user.email}`);
+        return;
+    }
+    res.redirect('/register');
 
 });
 
